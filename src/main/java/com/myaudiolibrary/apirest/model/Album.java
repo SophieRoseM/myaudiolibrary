56b8070ea48355a7057fc726f592b178fdd5d55c
Sophie <rose.sophie@hotmail.fr>
2019-01-08 14:37:50 +0100

    attributs, constructeur, getter et setter, liaison avec la BDD(ManyToOne)

--- src/main/java/com/myaudiolibrary/apirest/model/Album.java
+++ src/main/java/com/myaudiolibrary/apirest/model/Album.java
@@ -1,4 +1,48 @@
 package com.myaudiolibrary.apirest.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer albumId;
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "artistId")
+    private Artist artist;
+
+    public Album() {}
+
+    public Album(String title, Artist artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+
+    public Integer getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(Integer albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
 }
--- src/main/java/com/myaudiolibrary/apirest/model/Artist.java
+++ src/main/java/com/myaudiolibrary/apirest/model/Artist.java
@@ -1,4 +1,49 @@
 package com.myaudiolibrary.apirest.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer artistId;
+    private String name;
+
+    @OneToMany (mappedBy = "artist")
+    @JsonIgnoreProperties("artist")
+    private List<Album> albums;
+
+    public Artist() {}
+
+    public Artist(String name, List<Album> albums) {
+        this.name = name;
+        this.albums = albums;
+    }
+
+    public Integer getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(Integer artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(List<Album> albums) {
+        this.albums = albums;
+    }
 }
